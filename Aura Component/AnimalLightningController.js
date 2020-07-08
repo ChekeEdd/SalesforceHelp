a40987d74c69a155a694788de23742f30b87bde8
Eduard <67900093+ChekeEdd@users.noreply.github.com>
2020-07-08 13:27:10 +0300

    Add files via upload

--- Aura Component/AnimalLightningController.js
+++ Aura Component/AnimalLightningController.js
@@ -1,15 +1,19 @@
 ({
 	loadAnimals : function(component, event, helper) {
+         helper.tableInit(component);
 		 var action = component.get("c.getSameExternalIdAnimals");
          action.setParams({
-            animalId: component.get("v.recordId")
+            aID: component.get("v.recordId")
          });
-         console.log(component.get("v.recordId"));
-         action.setCallback(this, function(response){
-            var similarProperties = response.getReturnValue();
-            console.log(similarProperties);
-            component.set("v.similarProperties", similarProperties);
-         });
-        $A.enqueueAction(action);
-	}
+         helper.fetchAnimals(component,action);
+	},
+    onFilterClick : function(component, event, helper) {
+        var filterString =  component.get("v.filterValue");
+        var action = component.get("c.findAnimals");
+        action.setParams({
+            aID : component.get("v.recordId"),
+            searchanimal : component.get("v.filterValue")
+        })
+        helper.fetchAnimals(component,action);
+    }
 })
