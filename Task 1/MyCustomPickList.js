4a2e1d5544895c042b791b30f3a6065265a70e2e
Eduard <67900093+ChekeEdd@users.noreply.github.com>
2020-07-29 12:08:33 +0300

    Add files via upload

--- Task 1/MyCustomPickList.js
+++ Task 1/MyCustomPickList.js
@@ -1,20 +1,29 @@
 ({
     onFilterClick: function(component, event, helper) {
         var searchText = component.get('v.strName');
-        var typefield = component.get('v.Fields')
+        var typefield = component.get('v.Fields');
+        console.log(typefield);
         var action = component.get('c.findCase');
         action.setParams({KeyWord:searchText, typeField: typefield} );
         action.setCallback(this, function(response) {
-        var state = response.getState();
-        if (state === 'SUCCESS') {
-          var cases = response.getReturnValue().objects;
-          console.log(cases);
-        }
-             component.set("v.data", cases);
-            component.set('v.columns',[
-                {label: 'Opportunity name', fieldName: 'opportunityName', type: 'text'},
-            {label: 'Account name', fieldName: 'accountName', type: 'text'}] )
-      });
-      $A.enqueueAction(action);
+            var state = response.getState();
+            var cases;
+            if (state === 'SUCCESS') {
+                cases = response.getReturnValue().objects;
+                console.log(cases);
+            }
+            component.set("v.data", cases);
+            if(typefield !== 'CaseNumber'){
+            component.set('v.columns',[                
+                {label: 'Case Number', fieldName: 'CaseNumber', type: 'text'},
+                {label: typefield, fieldName: typefield, type: 'text'}] )
+                }
+            else{
+                component.set('v.columns',[                
+                {label: 'Case Number', fieldName: 'CaseNumber', type: 'text'}])
+            }
+        });
+        
+        $A.enqueueAction(action);
     }
 })
